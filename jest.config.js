6eb52544771c9e8a2e78dafc04004c392c4db9ff
Wil Bennett <wilbennettjr@live.com>
2019-09-16 08:40:32 -0400

    Added test config.

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "roots": [
+        "./src"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "coverageReporters": [
+        "html",
+        "text"
+    ],
+    // "setupFiles": [
+    //     "./src/twod/canvas-context/__mocks__/setup.ts"
+    // ]
+    // "automock": true
+}
