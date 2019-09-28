2fe50733a978257b1023f449ae09ddd39f8189a5
Wil Bennett <wilbennettjr@live.com>
2019-09-28 19:18:24 -0400

    Changed allowed value types.

--- src/objects/values/text-value.ts
+++ src/objects/values/text-value.ts
@@ -6,6 +6,14 @@ export class TextValue extends Value<string> {
     super(name, ValueType.string, "", undefined, value);
   }
 
+  get allowedValueTypes() {
+    return ValueType.bool
+      | ValueType.color
+      | ValueType.number
+      | ValueType.string
+      | ValueType.vector;
+  }
+
   protected convertToString(value: string) { return value; }
   protected convertFromString(value: string) { return value; }
 }
