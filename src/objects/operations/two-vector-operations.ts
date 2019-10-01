767205a6d4d7f728497895901768335e85e888e1
Wil Bennett <wilbennettjr@live.com>
2019-10-01 01:50:20 -0400

    Aliased function type.

--- src/objects/operations/two-vector-operations.ts
+++ src/objects/operations/two-vector-operations.ts
@@ -1,8 +1,10 @@
 import { Calc2VecFunc } from '..';
 import { calc2vec } from '../decorators';
 
+type T = Calc2VecFunc;
+
 // @ts-ignore - decorator implemented.
 class TwoVectorOperations {
-  @calc2vec("", "", "{p1} + {p2}") Add: Calc2VecFunc = (v1, v2) => v1.addN(v2);
-  @calc2vec("", "", "{p1} - {p2}") Subtract: Calc2VecFunc = (v1, v2) => v1.subN(v2);
+  @calc2vec("", "", "{p1} + {p2}") Add: T = (v1, v2) => v1.addN(v2);
+  @calc2vec("", "", "{p1} - {p2}") Subtract: T = (v1, v2) => v1.subN(v2);
 }
