bfeddeb2c7533f1700658325fe031354ac4a54ce
Wil Bennett <wilbennettjr@live.com>
2019-10-01 11:00:27 -0400

    Added reflection calculations.

--- src/objects/operations/two-vector-operations.ts
+++ src/objects/operations/two-vector-operations.ts
@@ -9,4 +9,8 @@ class TwoVectorOperations {
   Add: T = (v1, v2) => v1.addN(v2);
   @calc2vec("<b>{p1} - {p2}</b><br/>[{p1}.x - {p2}.x, {p1}.y - {p2}.y, {p1}.w - {p2}.w]", "", "", "{p1} - {p2}")
   Subtract: T = (v1, v2) => v1.subN(v2);
+  @calc2vec("<b>{p1} reflect {p2}</b>", "", "", "{p1} k {p2}")
+  Reflect: T = (v1, v2) => v2.reflectOffN(v1);
+  @calc2vec("<b>{p1} reflect off of {p2}</b>", "", "", "{p1} ʞ {p2}")
+  Reflect_off: T = (v1, v2) => v1.reflectOffN(v2);
 }
