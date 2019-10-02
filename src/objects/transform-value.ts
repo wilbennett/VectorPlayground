a4768bf37e43ac6c3f3518ef97042302d1fd667c
Wil Bennett <wilbennettjr@live.com>
2019-10-02 18:12:01 -0400

    Implemented TransformValue.

--- src/objects/transform-value.ts
+++ src/objects/transform-value.ts
@@ -1,3 +1,28 @@
-export class TransformValue {
+import { TransformObject, Value } from '.';
+import { Category, Tristate, ValueMode, ValueType } from '../core';
 
+export class TransformValue<T> extends Value<T> {
+  constructor(name: string, valueType: ValueType, defaultValue: T) {
+    super(name, valueType, defaultValue, Category.transformValue);
+
+    this.readOnlyText = true;
+    this._allowedModes = ValueMode.transform;
+    this.mode = ValueMode.transform;
+  }
+
+  transformValue(value: Tristate<T>): Tristate<T> {
+    if (!this.transform) return value;
+
+    value = this.transform.transform(value);
+
+    if (!this.modifier) return value;
+
+    value = this.modifier.transform(value);
+    return value;
+  }
+
+  // @ts-ignore - unused param.
+  protected convertToString(value: Tristate<T>): Tristate<string> { return ""; }
+  // @ts-ignore - unused param.
+  protected convertFromString(value: string): Tristate<TransformObject<T>> { return undefined; }
 }
