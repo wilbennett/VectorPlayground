a2da795481365b1d79f2d7b63fb6cd69c2543449
Wil Bennett <wilbennettjr@live.com>
2019-10-02 18:09:15 -0400

    Changed caption handling.

--- src/objects/transform-object.ts
+++ src/objects/transform-object.ts
@@ -1,15 +1,43 @@
 import { BaseObject } from '.';
 import { Category, Tristate, ValueType } from '../core';
-import { Utils } from '../utils';
 
-export abstract class TransformObject<T> extends BaseObject {
+export type TransformFunc<T> = (value: Tristate<T>) => Tristate<T>;
+
+export class TransformObject<T> extends BaseObject {
   constructor(name: string, valueType: ValueType, category?: Category) {
     super(name, category || Category.transform);
 
     this._valueType = valueType;
     this.isGlobal = false;
-    this.caption = Utils.capitalizeUnder(name);
   }
 
-  abstract transform(value: Tristate<T>): Tristate<T>;
+  transform(value: Tristate<T>): Tristate<T> { return value; }
+
+  protected calcOwnerText(owner: BaseObject) {
+    switch (owner.category) {
+      case Category.vectorObject:
+        return `${owner.caption}`;
+      default:
+        return `[${owner.caption}]`;
+    }
+  }
+
+  protected calcCaption() {
+    const owner = this.owner;
+
+    return owner
+      ? `${this.calcOwnerText(owner)}${this.captionRoot && ` ${this.captionRoot}`}`
+      : `${this.captionRoot}`;
+  }
+
+  protected setOwner(owner: BaseObject) {
+    super.setOwner(owner);
+
+    if (!this._caption)
+      this.caption = this.calcCaption();
+  }
+
+  protected ownerCaptionChanged() {
+    this.caption = this.calcCaption();
+  }
 }
